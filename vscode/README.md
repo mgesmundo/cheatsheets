d78060f8c2605064a9a0c76f37ff0ee0add84774
Marcello Gesmundo <mgesmundo@yoovant.com>
2015-05-16 21:04:51 +0200

    VSCode cheatsheet generator

---
+++ vscode/README.md
@@ -0,0 +1,20 @@
+# Prerequisites
+
+To generate the Microsoft Visual Studio Code cheat sheet you need NodeJS (>=0.10), Ruby and the *cheatset* gem.
+Please refer to [NodeJs](https://nodejs.org) and [Dash](https://github.com/Kapeli/cheatset/blob/master/README.md).
+
+# Installation
+
+To install the npm modules open your terminal and type:
+
+	$ npm install cheerio
+	$ npm install superagent
+
+# Usage
+
+To create the cheat sheet simply type on your terminal:
+
+	$ node vscode.js
+	$ cheatset generate vscode.rb
+
+Install *vscode.docset* in your Dash application. Done!
---
+++ vscode/vscode.js
@@ -0,0 +1,75 @@
+var cheerio = require('cheerio');
+var request = require('superagent');
+var fs = require('fs');
+var path = require('path');
+var html;
+var category;
+var command;
+var name;
+var notes;
+var $;
+var tables;
+var rows;
+var columns;
+var i, j;
+var categories = [
+  'Basic Editing',
+  'Rich Languages Editing',
+  'Navigation',
+  'Editor/Window Management',
+  'File Management',
+  'Display',
+  'Debug',
+  'Tasks'
+];
+var top = [
+  "cheatsheet do",
+  "  title 'Visual Studio Code'",
+  "  docset_file_name 'vscode'",
+  "  keyword 'vscode'",
+  "  introduction 'Microsoft Visual Studio Code cheat sheet'"
+].join('\n');
+var commands = '';
+var bottom = [
+  "  notes 'More information at https://code.visualstudio.com/Docs/customization'",
+  "end"
+].join('\n');
+var content;
+
+request
+  .get('https://code.visualstudio.com/Docs/customization')
+  .end(function (err, res) {
+    if (!err) {
+      // remove the scripts
+      html = res.text.replace(/<script[\w\W]*<\/script>/gm, '');
+      $ = cheerio.load(html);
+      // get all shotcuts tables
+      tables = $('.table-striped').find('tbody');
+      // console.log(tables.find('tr').eq(0).text());
+      for (i = 0; i < categories.length; i++) {
+        category = categories[i];
+        commands += "  category do\n";
+        commands += "    id '" + category + "'\n";
+        rows = $('tr', tables[i]);
+        for (j = 0; j < rows.length; j++) {
+          columns = $('td', rows[j]);
+          command = $('span', columns[0]).attr('data-osx') || 'unassigned';
+          command = command.replace(/\\/g,'\\\\');  	// escape '\'
+          name = $('td', rows[j]).eq(1).text();
+          notes = $('td', rows[j]).eq(2).text();  
+          commands += "    entry do\n";
+  	      commands += "      command '" + command + "'\n";
+  	      commands += "      name '" + name + "'\n";
+  	      commands += "      notes '" + notes + "'\n";
+          commands += "    end\n";    
+        }
+          commands += "  end\n";    
+      }
+      content = top + '\n' + commands + '\n' + bottom;
+      fs.writeFile(path.resolve('..','cheatsheets','vscode.rb'), content, function (err) {
+        if (!err) {
+          console.log('Done!\nConvert to docset typing:\n\n$ cheatset generate vscode.rb\n');
+        }
+      });
+    }
+  });
